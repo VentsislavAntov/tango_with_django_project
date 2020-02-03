4b67930eaeb4092f383db3918021b4bba0b93d23
Ventsislav Antov <2504299a@student.gla.ac.uk>
2020-02-03 23:53:54 +0000

    chapter 6 commit working

--- rango/views.py
+++ rango/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
-def index(request): 
+def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
 
 	context_dict = {}
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories'] = category_list
+	context_dict['pages'] = page_list
 
 	return render(request, 'rango/index.html', context=context_dict)
 
@@ -17,9 +19,9 @@ def about(request):
 	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 	return render(request, 'rango/about.html', context=context_dict)
 
+
 def show_category(request, category_name_slug):
 	context_dict = {}
-
 	try:
 		category = Category.objects.get(slug=category_name_slug)
 		pages = Page.objects.filter(category=category)
@@ -28,5 +30,16 @@ def show_category(request, category_name_slug):
 	except Category.DoesNotExist:
 		context_dict['categories'] = None
 		context_dict['pages'] = None
+	return render(request, 'rango/category.html', context=context_dict)
 
-	return render(request, 'rango/category.html', context = context_dict)
+
+def show_page(request, page_title):
+	context_dict = {}
+	try:
+		page = Page.objects.get(title=page_title)
+		context_dict['pages'] = page
+	except Page.DoesNotExist:
+		context_dict['pages'] = None
+
+	return render(request, 'rango/index.html', context=context_dict)
+
